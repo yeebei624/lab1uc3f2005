b8840d1050bf4aa7ace2465f4c972e1dca4e14f3
yeebei624 <72436204+yeebei624@users.noreply.github.com>
2020-10-09 10:27:27 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -19,6 +19,20 @@
           <th>Menara Standard Chartered, TPM</th>
           <th>03-22441234</th>
         </tr>
+        
+        <?php  
+        $connectionInfo = array("UID" => "APUAdmin", "pwd" => "Sixfourtwo624", "Database" => "APULab2Exercise",
+        "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+        $serverName = "tcp:lab2exercisetp000000.database.windows.net,1433";
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
+        //Establishes the connection
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if (!$conn)
+        {
+        die("Error connection: ".sqlsrv_errors());
+        }
+        ?>
+        
       </table>
     </center>
   </body>
