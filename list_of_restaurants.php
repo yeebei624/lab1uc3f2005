bd501c0422570f50156fded189e3c8f24c1933bb
yeebei624 <72436204+yeebei624@users.noreply.github.com>
2020-10-09 11:12:01 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -20,7 +20,22 @@
           <th>03-22441234</th>
         </tr>
         
-       <?php$serverName = "lab2exercisetp049935.database.windows.net"; $connectionOptions = array("Database" => "APULab2Exercise","Uid" => "APUAdmin","PWD" => "Sixfourtwo624");//Establishes the connection$conn = sqlsrv_connect($serverName, $connectionOptions);if (!$conn){die("Error connection: ".sqlsrv_errors());}$tsql= "SELECT * FROM [dbo].[restaurant]";$getResults= sqlsrv_query($conn, $tsql);if ($getResults == FALSE){die(sqlsrv_errors());}while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {echo "<tr>";echo "<td>". $row['restaurant_id'] . "</td>";echo "<td>". $row['restaurant_name'] ."</td>";echo "<td>". $row['restaurant_address'] . "</td>";echo "<td>". $row['restaurant_phone'] . "</td>";echo "</tr>";}sqlsrv_free_stmt($getResults);?>
+       <?php
+        $serverName = "lab2exercisetp049935.database.windows.net"; 
+        $connectionOptions = array("Database" => "APULab2Exercise","Uid" => "APUAdmin","PWD" => "Sixfourtwo624");
+        //Establishes the connection
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if (!$conn){die("Error connection: ".sqlsrv_errors());}
+        $tsql= "SELECT * FROM [dbo].[restaurant]";
+        $getResults= sqlsrv_query($conn, $tsql);
+        if ($getResults == FALSE){die(sqlsrv_errors());}
+        while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) 
+        {
+          echo "<tr>";echo "<td>". $row['restaurant_id'] . "</td>";
+          echo "<td>". $row['restaurant_name'] ."</td>";echo "<td>". $row['restaurant_address'] . "</td>";
+          echo "<td>". $row['restaurant_phone'] . "</td>";echo "</tr>";}
+        sqlsrv_free_stmt($getResults);
+        ?>
         
       </table>
     </center>
